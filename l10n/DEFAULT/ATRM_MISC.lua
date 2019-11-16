ccb44642c4b99c7028fb6939c426ec04686ca8a5
132nd-Entropy <ber17118@gmail.com>
2019-11-16 13:22:08 +0100

    NEW: add switchable AlDafra Air Defenses

---
+++ l10n/DEFAULT/ATRM_MISC.lua
@@ -0,0 +1,21 @@
+--- AlDafra SAM
+range_root_menu_misc = MENU_MISSION:New("Miscellaneous ",ParentMenu)
+local AlDafraSam = SPAWN:New("AL_DAHFRA_PATRIOT"):Spawn()
+
+function AlDafraSam_on()
+  misc2:Remove()
+  if AlDafraSam:IsAlive() then
+    AlDafraSam:SetAIOn()
+    misc1 = MENU_MISSION_COMMAND:New("Deactivate AlDafra AirDefenses",range_root_menu_misc,AlDafraSam_off)
+  end
+end
+
+function AlDafraSam_off()
+  misc1:Remove()
+  if AlDafraSam:IsAlive() then
+    AlDafraSam:SetAIOff()
+    misc2 = MENU_MISSION_COMMAND:New("Activate AlDafra AirDefenses",range_root_menu_misc,AlDafraSam_on)
+  end
+end
+misc1 = MENU_MISSION_COMMAND:New("Deactivate AlDafra AirDefenses",range_root_menu_misc,AlDafraSam_off)
+--- /AlDafra SAM
