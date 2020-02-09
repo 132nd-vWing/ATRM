225c4b4198725ffa533881e4149abf1bcf408bf2
132nd-Entropy <ber17118@gmail.com>
2020-02-09 16:25:23 +0100

    CHG: Set Tarawa and Escort to Green State, Set it to Spawn via the F10/Other/Misc menu

--- l10n/DEFAULT/ATRM_MISC.lua
+++ l10n/DEFAULT/ATRM_MISC.lua
@@ -1,6 +1,8 @@
 --- AlDafra SAM
 range_root_menu_misc = MENU_MISSION:New("Miscellaneous ",ParentMenu)
 local AlDafraSam = SPAWN:New("AL_DAHFRA_PATRIOT"):Spawn()
+local Tarawa = SPAWN:New("Tawara")
+local Merchants = SPAWN:New("Merchant")
 
 function AlDafraSam_on()
   misc2:Remove()
@@ -20,3 +22,14 @@ end
 misc1 = MENU_MISSION_COMMAND:New("Deactivate AlDafra AirDefenses",range_root_menu_misc,AlDafraSam_off)
 AlDafraSam_off()
 --- /AlDafra SAM
+
+--- AlDafra Spawn Tarawa
+function spawnTarawa()
+  Tarawa:OnSpawnGroup(function(_group)_group:OptionAlarmStateGreen()end)
+  Merchants:OnSpawnGroup(function(_group)_group:OptionAlarmStateGreen()end)
+  Tarawa:Spawn()
+  Merchants:Spawn()
+  misc3:Remove()
+end
+misc3 = MENU_MISSION_COMMAND:New("Spawn Ships for Helicopter Training",range_root_menu_misc,spawnTarawa)
+---/AlDafra Spawn Tarawa
