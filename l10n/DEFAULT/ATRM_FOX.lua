2273b54939540246f103f853457e49c12f1a8852
132nd-Entropy <ber17118@gmail.com>
2020-02-17 16:00:02 +0100

    NEW: Replace DATIS with MOOSE-ATIS CHG: Change Missile explosion Distance from 60m to 200m for FOX-script CHG: Remove Voiceovers on the Range (requested by @Neck)

--- l10n/DEFAULT/ATRM_FOX.lua
+++ l10n/DEFAULT/ATRM_FOX.lua
@@ -1,6 +1,6 @@
 -- FOX script (replaces Missiletrainer)
 fox=FOX:New()
-fox:SetExplosionDistance(60)
+fox:SetExplosionDistance(200)
 fox:SetDisableF10Menu(true)
 fox:SetDefaultLaunchAlerts(false)
 fox:Start()
--- l10n/DEFAULT/ATRM_MISC.lua
+++ l10n/DEFAULT/ATRM_MISC.lua
@@ -1,3 +1,65 @@
+--- FOX script (replaces Missiletrainer)
+fox=FOX:New()
+fox:SetExplosionDistance(200)
+fox:SetDisableF10Menu(true)
+fox:SetDefaultLaunchAlerts(false)
+fox:Start()
+---/Fox
+
+
+--- ATIS
+atisAlDhafra=ATIS:New("Al Dhafra AB", 126.400)
+atisAlDhafra:SetTowerFrequencies(126.200)
+atisAlDhafra:SetTACAN(96)
+atisAlDhafra:SetMapMarks(true)
+--atisAlDhafra:SetVOR(114.9)
+
+--atisAlDhafra_active = atisAlDhafra:GetActiveRunway()
+--
+--if atisAlDhafra_active == "13" then
+--atisAlDhafra:SetActiveRunway("13R") else
+--atisAlDhafra:SetActiveRunway("31L")
+--end
+atisAlDhafra:Start()
+
+
+
+atisAlAin=ATIS:New("Al Ain International Airport", 119.400)
+atisAlAin:SetTowerFrequencies(119.200)
+atisAlAin:SetMapMarks(true)
+atisAlAin:Start()
+
+
+atisAlMinhad=ATIS:New("Al Minhad AB", 121.400)
+atisAlMinhad:SetTowerFrequencies(121.200)
+atisAlMinhad:SetMapMarks(true)
+atisAlMinhad:Start()
+
+atisLiwa=ATIS:New("Liwa Airbase", 119.300)
+atisLiwa:SetTowerFrequencies(119.300)
+atisLiwa:SetMapMarks(true)
+atisLiwa:Start()
+
+atisSasAlNakheel=ATIS:New("Sas Al Nakheel Airport", 128.400)
+atisSasAlNakheel:SetTowerFrequencies(128.200)
+atisSasAlNakheel:SetMapMarks(true)
+atisSasAlNakheel:Start()
+
+atisRasAlKaimah=ATIS:New("Ras Al Khaimah", 118.400)
+atisRasAlKaimah:SetTowerFrequencies(118.200)
+atisRasAlKaimah:SetMapMarks(true)
+atisRasAlKaimah:Start()
+
+atisFujairah=ATIS:New("Fujairah Intl", 124.400)
+atisFujairah:SetTowerFrequencies(124.200)
+atisFujairah:SetMapMarks(true)
+atisFujairah:Start()
+
+--- ATIS
+
+
+
+
 --- AlDafra SAM
 range_root_menu_misc = MENU_MISSION:New("Miscellaneous ",ParentMenu)
 local AlDafraSam = SPAWN:New("AL_DAHFRA_PATRIOT"):Spawn()
