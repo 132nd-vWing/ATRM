875ca5718d9312185939178ed4497e523d1c7388
132nd-Entropy <ber17118@gmail.com>
2020-07-23 12:02:48 +0200

    NEW: Add Tarawa Group to the NavyGroups Script and add the option, after spawning the group via menu, to turn it into the wind

--- l10n/DEFAULT/ATRM_MISC.lua
+++ l10n/DEFAULT/ATRM_MISC.lua
@@ -134,8 +134,6 @@ atis_Fujairah_menu = MENU_MISSION_COMMAND:New("Start ATIS Fujairah Intl",atismen
 --- AlDafra SAM
 
 local AlDafraSam = SPAWN:New("AL_DAHFRA_PATRIOT"):Spawn()
-local Tarawa = SPAWN:New("Tawara")
-local Merchants = SPAWN:New("Merchant")
 
 function AlDafraSam_on()
   misc2:Remove()
@@ -156,13 +154,4 @@ misc1 = MENU_MISSION_COMMAND:New("Deactivate AlDafra AirDefenses",range_root_men
 AlDafraSam_off()
 --- /AlDafra SAM
 
---- AlDafra Spawn Tarawa
-function spawnTarawa()
-  Tarawa:OnSpawnGroup(function(_group)_group:OptionAlarmStateGreen()end)
-  Merchants:OnSpawnGroup(function(_group)_group:OptionAlarmStateGreen()end)
-  Tarawa:Spawn()
-  Merchants:Spawn()
-  misc3:Remove()
-end
-misc3 = MENU_MISSION_COMMAND:New("Spawn Ships for Helicopter Training",range_root_menu_misc,spawnTarawa)
----/AlDafra Spawn Tarawa
+
--- l10n/DEFAULT/ATRM_NavyGroups.lua
+++ l10n/DEFAULT/ATRM_NavyGroups.lua
@@ -1,3 +1,6 @@
+carrier_root_menu = MENU_MISSION:New("Navy Groups")
+
+--- CVN73 Supercarrier ---
 CVN73 = NAVYGROUP:New("CVN-73")
 CVN73:SetPatrolAdInfinitum()
 CVN73:SetOptionROE(4)
@@ -11,9 +14,41 @@ function start_recovery()
     local timerecovery_start = UTILS.SecondsToClock(timenow,true)
     timerecovery_end = UTILS.SecondsToClock(timeend,true)
     CVN73:AddTurnIntoWind(timerecovery_start,timerecovery_end,15,true,-9)
-    MessageToAll("Carrier Turning, at time "..timerecovery_start.." until "..timerecovery_end)
+    MessageToAll("CVN73 is turning, Recovery Window open from time "..timerecovery_start.." until "..timerecovery_end)
   end
 end
-
-carrier_root_menu = MENU_MISSION:New("Navy Groups")
 menu_start_recovery = MENU_MISSION_COMMAND:New("Start Recovery at CVN73",carrier_root_menu,start_recovery)
+--- CVN73 Supercarrier ---
+
+
+
+--- LHA-1 Tarawa and merchant ships ---
+tarawa = NAVYGROUP:New("Tarawa")
+tarawa:SetPatrolAdInfinitum()
+tarawa:SetOptionROE(4)
+merchant = NAVYGROUP:New("Merchant")
+merchant:SetPatrolAdInfinitum()
+merchant:SetOptionROE(4)
+
+
+function spawnTarawa()
+  tarawa:Activate()
+  merchant:Activate()
+  Spawn_Tarawa:Remove()
+
+  function start_recovery_tarawa()
+    if tarawa:IsSteamingIntoWind() == true then
+      MessageToAll("LHA-1 Tarawa is currently recovering, recovery window closes at time "..timerecovery_end_tarawa)
+    else
+      local timenow_tarawa=timer.getAbsTime( )
+      local timeend_tarawa=timenow_tarawa+90*60
+      local timerecovery_start_tarawa = UTILS.SecondsToClock(timenow_tarawa)
+      timerecovery_end_tarawa = UTILS.SecondsToClock(timeend_tarawa,true)
+      tarawa:AddTurnIntoWind(timerecovery_start_tarawa,timerecovery_end_tarawa,15,true)
+      MessageToAll("LHA-1 Tarawa is turning, Recovery Window open from time "..timerecovery_start_tarawa.." until "..timerecovery_end_tarawa)
+    end
+  end
+  menu_start_recovery_tarawa = MENU_MISSION_COMMAND:New("Start Recovery at LHA-1 Tarawa",carrier_root_menu,start_recovery_tarawa)
+end
+Spawn_Tarawa = MENU_MISSION_COMMAND:New("Spawn LHA-1 Tarawa and Merchant Ships",carrier_root_menu,spawnTarawa)
+--- LHA-1 Tarawa and merchant ships ---
