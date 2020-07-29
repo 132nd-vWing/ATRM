737ef20b95cd15ddf0fc9df00335ae734146a18b
132nd-Entropy <ber17118@gmail.com>
2020-07-29 16:25:51 +0200

    FIX apply fixes for CVN71

--- l10n/DEFAULT/ATRM_NavyGroups.lua
+++ l10n/DEFAULT/ATRM_NavyGroups.lua
@@ -15,9 +15,9 @@ SCHEDULER:New(nil,function()
   if CVN_73_beacon_unit then
     CVN73_Beacon = CVN_73_beacon_unit:GetBeacon()
     CVN73_Beacon:ActivateTACAN(73,"X","C73",true)
-    env.info("CVN73 beacon and Tacan refreshed")
+    env.info("CVN73 Tacan refreshed")
   end
-end,{},5,120)
+end,{},5,5*60)
 
 function start_recovery()
   if CVN73:IsSteamingIntoWind() == true then
@@ -33,6 +33,43 @@ function start_recovery()
 end
 menu_start_recovery = MENU_MISSION_COMMAND:New("Start Recovery at CVN73",carrier_root_menu,start_recovery)
 --- CVN73 Supercarrier ---
+
+
+--- CVN71 Red Supercarrier ---
+CVN71 = NAVYGROUP:New("CVN71_Theodore Roosevelt")
+CVN71:SetPatrolAdInfinitum()
+CVN71:SetOptionROE(4)
+CVN_71_beacon_unit = UNIT:FindByName("CVN71_Theodore Roosevelt")
+if CVN_71_beacon_unit then
+  CVN71_Beacon = CVN_71_beacon_unit:GetBeacon()
+  CVN71_Beacon:ActivateICLS(11,"C71")
+  env.info("CVN71 ICLS started on channel 11")
+end
+
+SCHEDULER:New(nil,function()
+  if CVN_71_beacon_unit then
+    CVN71_Beacon = CVN_71_beacon_unit:GetBeacon()
+    CVN71_Beacon:ActivateTACAN(71,"X","C71",true)
+    env.info("CVN71 Tacan refreshed")
+  end
+end,{},5,20*60)
+
+
+function start_recovery_71()
+  if CVN71:IsSteamingIntoWind() == true then
+    MessageToAll("CVN-71 is currently recovering, recovery window closes at time "..timerecovery_end_71)
+  else
+    local timenow_71=timer.getAbsTime( )
+    local timeend_71=timenow_71+45*60
+    local timerecovery_start_71 = UTILS.SecondsToClock(timenow_71,true)
+    timerecovery_end_71 = UTILS.SecondsToClock(timeend_71,true)
+    CVN71:AddTurnIntoWind(timerecovery_start_71,timerecovery_end_71,25,true,-9)
+    MessageToAll("CVN71 is turning, Recovery Window open from time "..timerecovery_start_71.." until "..timerecovery_end_71)
+  end
+end
+menu_start_recovery_71 = MENU_MISSION_COMMAND:New("Start Recovery at CVN71 (Red Supercarrier)",carrier_root_menu,start_recovery_71)
+--- CVN71 Red Supercarrier ---
+
 
 
 
@@ -66,22 +103,3 @@ Spawn_Tarawa = MENU_MISSION_COMMAND:New("Spawn LHA-1 Tarawa and Merchant Ships",
 --- LHA-1 Tarawa and merchant ships ---
 
 
---- CVN71 Red Supercarrier ---
-CVN71 = NAVYGROUP:New("CVN71_Theodore Roosevelt")
-CVN71:SetPatrolAdInfinitum()
-CVN71:SetOptionROE(4)
-
-function start_recovery_71()
-  if CVN71:IsSteamingIntoWind() == true then
-    MessageToAll("CVN-71 is currently recovering, recovery window closes at time "..timerecovery_end_71)
-  else
-    local timenow_71=timer.getAbsTime( )
-    local timeend_71=timenow_71+90*60
-    local timerecovery_start_71 = UTILS.SecondsToClock(timenow_71,true)
-    timerecovery_end_71 = UTILS.SecondsToClock(timeend_71,true)
-    CVN71:AddTurnIntoWind(timerecovery_start_71,timerecovery_end_71,25,true,-9)
-    MessageToAll("CVN71 is turning, Recovery Window open from time "..timerecovery_start_71.." until "..timerecovery_end_71)
-  end
-end
-menu_start_recovery_71 = MENU_MISSION_COMMAND:New("Start Recovery at CVN71 (Red Supercarrier)",carrier_root_menu,start_recovery_71)
---- CVN71 Red Supercarrier ---
