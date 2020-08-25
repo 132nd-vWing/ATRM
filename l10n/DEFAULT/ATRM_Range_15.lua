b51c4f0ded94fdadd9125c138bfd290330f7c356
132nd-Entropy <ber17118@gmail.com>
2020-08-25 16:39:31 +0200

    NEW: UPDATE MOOSE CHG: reinforcements can be called in 3x on Range 15 Attack on Madrah CHG: Red Carrier and RED AWACS moved to the WEST to facilitate A-A training for naval sqds

--- l10n/DEFAULT/ATRM_Range_15.lua
+++ l10n/DEFAULT/ATRM_Range_15.lua
@@ -20,15 +20,38 @@ range_15_menu_AR_red = MENU_MISSION_COMMAND:New("Activate AR (SCUD hunt) RED",ra
 
 
 
+local SPAWN_15_1 = SPAWN:New("R15_AR_Recce_1")
+local SPAWN_15_2 = SPAWN:New("R15_AR_Recce_2")
+local SPAWN_15_3 = SPAWN:New("R15_AR_MBT_PLT1")
+local SPAWN_15_4 = SPAWN:New("R15_AR_IFV_PLT2")
+local SPAWN_15_5 = SPAWN:New("R15_AR_IFV_PLT3")
+local SPAWN_15_6 = SPAWN:New("R15_AR_IFV_PLT4")
+
+local function range_15_AR_Mahdah_reattack()
+  range15_respawn_timer = range15_respawn_timer+1
+  if range15_respawn_timer < 4 then
+    SPAWN_15_1:Spawn()
+    SPAWN_15_2:Spawn()
+    SPAWN_15_3:Spawn()
+    SPAWN_15_4:Spawn()
+    SPAWN_15_5:Spawn()
+    SPAWN_15_6:Spawn()
+  else if range_15_menu_AR_Mahdah_reattack then range_15_menu_AR_Mahdah_reattack:Remove()
+    end
+  end
+end
+
 
 local function range_15_AR_Mahdah()
   range_15_menu_AR_Mahdah:Remove()
-  SPAWN:New("R15_AR_Recce_1"):Spawn()
-  SPAWN:New("R15_AR_Recce_2"):Spawn()
-  SPAWN:New("R15_AR_MBT_PLT1"):Spawn()
-  SPAWN:New("R15_AR_IFV_PLT2"):Spawn()
-  SPAWN:New("R15_AR_IFV_PLT3"):Spawn()
-  SPAWN:New("R15_AR_IFV_PLT4"):Spawn()
+  range_15_menu_AR_Mahdah_reattack = MENU_MISSION_COMMAND:New("Range 15 AR Attack on Mahdah, spawn reinforcements",range_15_menu_root,range_15_AR_Mahdah_reattack)
+  SPAWN_15_1:Spawn()
+  SPAWN_15_2:Spawn()
+  SPAWN_15_3:Spawn()
+  SPAWN_15_4:Spawn()
+  SPAWN_15_5:Spawn()
+  SPAWN_15_6:Spawn()
+  range15_respawn_timer = 1
 end
 
 range_15_menu_AR_Mahdah = MENU_MISSION_COMMAND:New("Range 15 AR Attack on Mahdah",range_15_menu_root,range_15_AR_Mahdah)
