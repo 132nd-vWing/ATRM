44112710d65f9923d473bd27e30f790f59ae4f52
132nd-Entropy <entropysg@gmail.com>
2020-11-27 14:00:31 +0100

    NEW: Add Assaf's Cas Preset scenarios to the Range14 menu structure

--- l10n/DEFAULT/ATRM_Range_14.lua
+++ l10n/DEFAULT/ATRM_Range_14.lua
@@ -170,147 +170,36 @@ end
 
 range_14_menu = MENU_MISSION_COMMAND:New("Activate CAS scenario 1",range_14_menu_root,range_14)
 
----BASIC AR Tasking at Range14
---local function range_14_AR()
---  trigger.action.setUserFlag(101,true)
---  range_14_AR_menu:Remove()
---  local range_14_AR_Groups = {
---      "21st_MBT_RGT_1_1_1";
---      "21st_MBT_RGT_1_1_2";
---      "21st_MBT_RGT_2_1_1";
---      "21st_MBT_RGT_1_1_2";
---      "21st_MBT_RGT_2_2_1";
---      "21st_MBT_RGT_2_2_2";
---      "21st_MBT_RGT_AD_batt_1_1";
---      "21st_MBT_RGT_AD_batt_1_2";
---      "21st_MBT_RGT_AD_batt_2_1";
---      "21st_MBT_RGT_AD_batt_2_2";
---      "21st_MBT_RGT_AD_batt_2_3";
---      "21st_MBT_RGT_AD_batt_2_4";
---      "21st_MBT_RGT_AD_radar";
---      "21st_MBT_RGT_arty_batt1";
---      "21st_MBT_RGT_arty_batt2";
---      "21st_MBT_RGT_support_special";
---      "21st_Support";
---      "21st_Support_ammo";
---      "21st_Support_ammo2";
---      "21st_Support_food1";
---      "21st_Support_food2";
---      "21st_Support_fuel1";
---      "21st_Support_fuel2";
---  }
---
---  for i,_group in pairs(range_14_AR_Groups) do
---        SPAWN:New(_group):InitAIOff():Spawn()
---      env.info("spawning ".._group)
---    end
---  local range_14_threatsites = {
---    "21st_MBT_RGT_AD_batt_1_1";
---    "21st_MBT_RGT_AD_batt_1_2";
---    "21st_MBT_RGT_AD_batt_2_1";
---    "21st_MBT_RGT_AD_batt_2_2";
---    "21st_MBT_RGT_AD_batt_2_3";
---    "21st_MBT_RGT_AD_batt_2_4";
---  }
---
---
---  --IADS
---  local evasion_for_client_planes_only = true
---  local chance_for_evasive_action = 100
---  local Target_Smoke = false
---  local chance_for_group_relocating = 0
---  local relocating_distance = 500
---  local evasion_delay = math.random(2,6)
---  local radar_delay = math.random(60,180)
---  local move_distance = 0
---  local _evadeRadars_R14 = {}
---  local SEAD_enabled_Sams_R14 = SET_GROUP:New()
---
---  for i,_groupname in ipairs(range_14_threatsites) do
---    local _group = SPAWN:New(_groupname):Spawn()
---    SEAD_enabled_Sams_R14:AddGroup(_group)
---  end
---  BASE:HandleEvent(EVENTS.Shot)
---  SEAD_enabled_Sams_R14:ForEachGroupAlive(
---    function(_group)
---      local Sam_group_name = _group:GetName()
---      env.info("Sam GROUP Name is "..Sam_group_name)
---      Sam_units = _group:GetUnits()
---      for i,_unit in ipairs(Sam_units) do
---        if
---          _unit:HasAttribute("SAM SR") or _unit:HasAttribute("SAM TR")
---        then
---          env.info("Radar detected for UNIT ".._unit:GetName())
---          table.insert(_evadeRadars_R14,_unit:GetName())
---        else
---        end
---      end
---    end
---  )
---  if evasion_for_client_planes_only == true
---  then
---    function BASE:OnEventShot(EventData)
---      local clientplane = EventData.IniPlayerName
---      if clientplane ~= nil
---      then
---        env.info("a missile has been shot by "..clientplane)
---        local SEAD_Weapon_Name = EventData.Weapon:getTypeName()
---        if SEAD_Weapon_Name == "weapons.missiles.AGM_88" then
---          local SEAD_Target = EventData.Weapon:getTarget()
---          local SEAD_Target_Name = Unit.getName(SEAD_Target)
---          local SEAD_Target_Unit = UNIT:FindByName(SEAD_Target_Name)
---          local SEAD_Target_GROUP = SEAD_Target_Unit:GetGroup()
---          local SEAD_Shooter_Unit = EventData.IniUnit
---          local SEAD_Shooter_Name = SEAD_Shooter_Unit:GetName()
---          for _,evasive_radar in pairs(_evadeRadars_R14) do
---            if evasive_radar == SEAD_Target_Name
---            then
---              env.info(SEAD_Shooter_Name.." has fired "..SEAD_Weapon_Name.." at "..SEAD_Target_Name)
---              env.info("AGM_88 shot detected from  "..SEAD_Shooter_Name.." on "..SEAD_Target_Name)
---              if math.random(1,140) <= chance_for_evasive_action
---              then
---                Radar_Unit_Evasive_Action(SEAD_Target_Unit)
---              end
---            end
---          end
---        end
---      end
---    end
---  end
---
---  function Radar_Unit_Evasive_Action(_unit) -- define the evasive action of the SAMsite when shot at by a HARM
---    if Target_Smoke == true then
---      _unit:SmokeRed()
---  end
---  local evasion_delay = math.random(3,8)
---  env.info("Sam waiting "..evasion_delay.."seconds before taking evasive measure")
---  SCHEDULER:New(nil,
---    function()
---      if  math.random(1,140) <= chance_for_group_relocating
---      then
---        env.info(_unit:GetName().." now taking evasive action")
---        env.info(_unit:GetName().."Air Defemse System now relocating "..relocating_distance.." meters")
---        _unit:OptionAlarmStateGreen()
---        local _groupcoordinate = _unit:GetCoordinate()
---        local _tocoordinate = _groupcoordinate:Translate( relocating_distance, math.random(3514) )
---        local _ToCoord_vec2 = _tocoordinate:GetVec2()
---        _unit:TaskRouteToVec2( _ToCoord_vec2 )
---        _unit:OptionAlarmStateGreen()
---        local radarbackon = SCHEDULER:New(nil,
---          function()
---            _unit:OptionAlarmStateRed()
---            env.info("radar back on")
---          end,{},radar_delay)
---      else
---        env.info(_unit:GetName().." switching off Radar")
---        _unit:OptionAlarmStateGreen()
---        local radarbackon = SCHEDULER:New(nil,
---          function()
---            _unit:OptionAlarmStateRed()
---            env.info("radar back on")
---          end,{},radar_delay)
---      end
---    end,{},evasion_delay)
---  end
---end
---range_14_AR_menu = MENU_MISSION_COMMAND:New("EXGS - Activate AR R14",range_14_menu_root,range_14_AR)
+local function range14_flag111()
+range_14_menu_flag111:Remove()
+range_14_menu_flag112:Remove()
+range_14_menu_flag111_112:Remove()
+trigger.action.setUserFlag(111, true )
+MessageToAll("R14 CAS scenario 1 activated")
+end
+
+local function range14_flag112()
+trigger.action.setUserFlag(111, true )
+range_14_menu_flag111:Remove()
+range_14_menu_flag112:Remove()
+range_14_menu_flag111_112:Remove()
+MessageToAll("R14 CAS scenario 2 activated")
+end
+
+local function range14_flag_111_112()
+if math.random(1,2) == 1 then
+trigger.action.setUserFlag(111, true )
+range_14_menu_flag111:Remove()
+range_14_menu_flag112:Remove()
+range_14_menu_flag111_112:Remove()
+else
+trigger.action.setUserFlag(112, true )
+range_14_menu_flag111:Remove()
+range_14_menu_flag112:Remove()
+range_14_menu_flag111_112:Remove()
+end
+end
+
+range_14_menu_flag111 = MENU_MISSION_COMMAND:New("Activate Range-14 preset CAS scenario 1",range_14_menu_root,range14_flag111)
+range_14_menu_flag112 = MENU_MISSION_COMMAND:New("Activate Range-14 preset CAS scenario 2",range_14_menu_root,range14_flag112)
+range_14_menu_flag111_112 = MENU_MISSION_COMMAND:New("Activate Range-14 preset CAS scenario randomly",range_14_menu_root,range14_flag_111_112)
