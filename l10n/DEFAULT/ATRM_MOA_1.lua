d79e9a9141e50edccdd83be3a40aaa305a62c682
132nd-Entropy <entropysg@gmail.com>
2021-02-04 16:46:21 +0100

    NEW: make redair in MOA1 respawnable NEW: add option to spawn redair in MOA1 passively (drones that fly a racetrack), or actively (they will engage)

--- l10n/DEFAULT/ATRM_MOA_1.lua
+++ l10n/DEFAULT/ATRM_MOA_1.lua
@@ -1,70 +1,31 @@
-
 function moa_1_SpawnMigs_passive()
-  local moa1zone = ZONE_POLYGON:New("moa1zone",GROUP:FindByName("MOA1"))
-  local flightgroup=FLIGHTGROUP:New("redair_moa1_mig29")
   local orbitstart = GROUP:FindByName("orbitstart_mig29")
   local orbitstartlocation = orbitstart:GetCoordinate()
-  flightgroup:SetEngageDetectedOn(80, {"Air"}, moa1zone)
-  local mission=AUFTRAG:NewORBIT_RACETRACK(orbitstartlocation,30000,400,90,20)
-  flightgroup:AddMission(mission)
-
-
-  function flightgroup:onafterEngageTarget(From,Event,To,Target)
-    local target=Target
-    -- Message to everybody and in the DCS log file.
-    local text=string.format("Engaged group %s", target:GetName())
-    --MESSAGE:New(text, 120,flightgroup:GetName()):ToAll()
-    env.info(text)
-  end
-
-  function flightgroup:OnAfterDetectedGroupNew(From, Event, To, Group)
-    local group=Group --Wrapper.Group#GROUP
-    -- Message to everybody and in the DCS log file.
-    local text=string.format("Detected group %s", group:GetName())
-    --MESSAGE:New(text, 120,flightgroup:GetName()):ToAll()
-    env.info(text)
-
-    env.info("red Mig spawned in Moa1")
-  end
+  local cap1 = AUFTRAG:NewORBIT_RACETRACK(orbitstartlocation,30000,500,90,15)
+  local mig291 = SPAWN:New("redair_moa1_mig29")
+  local mig29211 = mig291:Spawn()
+  local FG1 = FLIGHTGROUP:New(mig29211)  
+  FG1:AddMission(cap1)
 end
 
 function moa_1_SpawnMigs_active()
-  local PatrolZone = ZONE_POLYGON:New( "MOA1_patrolzone", GROUP:FindByName("MOA1_patrolzone") )
-  local moa1zone = ZONE_POLYGON:New("moa1zone",GROUP:FindByName("MOA1"))
-  local flightgroup=FLIGHTGROUP:New("redair_moa1_mig29")
   local orbitstart = GROUP:FindByName("orbitstart_mig29")
   local orbitstartlocation = orbitstart:GetCoordinate()
-  flightgroup:SetEngageDetectedOn(80, {"Air"}, moa1zone)
-
-  local mission=AUFTRAG:NewPATROLZONE(PatrolZone, 400, 30000)
-  flightgroup:AddMission(mission)
-
-
-  function flightgroup:onafterEngageTarget(From,Event,To,Target)
-    local target=Target
-    -- Message to everybody and in the DCS log file.
-    local text=string.format("Engaged group %s", target:GetName())
-    -- Create an Intercept mission.
-    local mission_intercept=AUFTRAG:NewINTERCEPT(target)
-    flightgroup:AddMission(mission_intercept)
-    MESSAGE:New(text, 120,flightgroup:GetName()):ToAll()
-    env.info(text)
+  local MOA1_capzone1 = ZONE_RADIUS:New("MOA1_capzone1",ZONE:New("MOA1_engagezone1"):GetVec2(),56174)
+  local cap2 = AUFTRAG:NewCAP(MOA1_capzone1,30000,500,orbitstartlocation,90,15)
+  local mig292 = SPAWN:New("redair_moa1_mig29")
+  local mig29212 = mig292:Spawn()
+  local FG2 = FLIGHTGROUP:New(mig29212)  
+  --FG2:AddMission(cap2)
+  FG2:AddMission(cap2)
   end
 
-  function flightgroup:OnAfterDetectedGroupNew(From, Event, To, Group)
-    local group=Group --Wrapper.Group#GROUP
-    -- Message to everybody and in the DCS log file.
-    local text=string.format("Detected group %s", group:GetName())
-    MESSAGE:New(text, 120,flightgroup:GetName()):ToAll()
-    env.info(text)
-
-    env.info("red Mig spawned in Moa1")
-  end
-end
+moa_1_menu_Migs1 = MENU_MISSION_COMMAND:New("Spawn Red Mig29s in MOA 1 passive",moa_root_menu,moa_1_SpawnMigs_passive)
+moa_1_menu_Migs2 = MENU_MISSION_COMMAND:New("Spawn Red Mig29s in MOA 1 active",moa_root_menu,moa_1_SpawnMigs_active)
 
 
-moa_1_menu_Migs = MENU_MISSION_COMMAND:New("Spawn Red Mig29s in MOA 1 passive",moa_root_menu,moa_1_SpawnMigs_passive)
---moa_1_menu_Migs = MENU_MISSION_COMMAND:New("Spawn Red Mig29s in MOA 1 active",moa_root_menu,moa_1_SpawnMigs_active)
+
+
 
 
 
