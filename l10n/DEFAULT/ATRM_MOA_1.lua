a757e6f9001d55599852148629ce5f31350f1993
132nd-Entropy <entropysg@gmail.com>
2021-02-09 13:26:18 +0100

    FIX: improve redair script on Moa1, the engage zone is now a polygon ADD: add the option to instantly destroy all red air in MOA1

--- l10n/DEFAULT/ATRM_MOA_1.lua
+++ l10n/DEFAULT/ATRM_MOA_1.lua
@@ -1,27 +1,476 @@
-function moa_1_SpawnMigs_passive()
-  local orbitstart = GROUP:FindByName("orbitstart_mig29")
-  local orbitstartlocation = orbitstart:GetCoordinate()
-  local cap1 = AUFTRAG:NewORBIT_RACETRACK(orbitstartlocation,30000,500,90,15)
-  local mig291 = SPAWN:New("redair_moa1_mig29")
-  local mig29211 = mig291:Spawn()
-  local FG1 = FLIGHTGROUP:New(mig29211)  
-  FG1:AddMission(cap1)
+local Moa1_Migs = {}
+local zoneCAP=ZONE_POLYGON:NewFromGroupName("MOA1_patrolzone")
+local zoneOrbit=ZONE_POLYGON:NewFromGroupName("MOA1_Orbit_Zone")
+local mission_racetrack=AUFTRAG:NewORBIT_RACETRACK(zoneOrbit:GetRandomCoordinate(), 26000, 400, 90, 20)
+local mig29 = SPAWN:New("redair_moa1_mig29")
+
+
+local function flightgroupMiG(group)
+  table.insert(Moa1_Migs,group:GetName())
+  local flightgroup=FLIGHTGROUP:New(group)
+  flightgroup:SetEngageDetectedOn(60, {"Air"}, zoneCAP)
+  flightgroup:AddMission(mission_racetrack)
+  function flightgroup:OnAfterDetectedGroupNew(From, Event, To, Group)
+    local group=Group --Wrapper.Group#GROUP
+    -- Message to everybody and in the DCS log file.
+    local text=string.format("Detected group %s", group:GetName())
+    MESSAGE:New(text, 120,flightgroup:GetName()):ToAll()
+    env.info(text)
+  end
 end
 
-function moa_1_SpawnMigs_active()
-  local orbitstart = GROUP:FindByName("orbitstart_mig29")
-  local orbitstartlocation = orbitstart:GetCoordinate()
-  local MOA1_capzone1 = ZONE_RADIUS:New("MOA1_capzone1",ZONE:New("MOA1_engagezone1"):GetVec2(),56174)
-  local cap2 = AUFTRAG:NewCAP(MOA1_capzone1,30000,500,orbitstartlocation,90,15)
-  local mig292 = SPAWN:New("redair_moa1_mig29")
-  local mig29212 = mig292:Spawn()
-  local FG2 = FLIGHTGROUP:New(mig29212)  
-  --FG2:AddMission(cap2)
-  FG2:AddMission(cap2)
-  end
 
-moa_1_menu_Migs1 = MENU_MISSION_COMMAND:New("Spawn Red Mig29s in MOA 1 passive",moa_root_menu,moa_1_SpawnMigs_passive)
-moa_1_menu_Migs2 = MENU_MISSION_COMMAND:New("Spawn Red Mig29s in MOA 1 active",moa_root_menu,moa_1_SpawnMigs_active)
+local function destroyallmigs()
+  if #Moa1_Migs > 0 then
+    for i, _group in pairs( Moa1_Migs) do
+      GROUP:FindByName(_group):Destroy()
+      env.info("Group ".._group.." destroyed")
+      local Moa1_Migs = {}
+    end
+  else
+    env.info("no groups found for deletion")
+  end
+end
+
+local function moa_1_SpawnMigs_active()
+  mig29:OnSpawnGroup(flightgroupMiG)
+  mig29:Spawn()
+end
+
+moa_1_menu_Migs_spawn = MENU_MISSION_COMMAND:New("Spawn Red Mig29 in MOA 1",moa_root_menu,moa_1_SpawnMigs_active)
+moa_1_menu_Migs_destroy = MENU_MISSION_COMMAND:New("Destroy all Mig29s in MOA 1",moa_root_menu,destroyallmigs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
