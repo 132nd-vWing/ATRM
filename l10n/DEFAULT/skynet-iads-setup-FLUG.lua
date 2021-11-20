484a3c88c9c041da85bcb4809d104c9f4ed484ee
132nd.Neck <neck.dcs@gmail.com>
2021-11-20 23:10:00 +0100

    chg: troubleshoot skynet

--- l10n/DEFAULT/skynet-iads-setup-FLUG.lua
+++ l10n/DEFAULT/skynet-iads-setup-FLUG.lua
@@ -1,7 +1,7 @@
 do
 
 --create an instance of the IADS
-redIADS = SkynetIADS:create(FLUG)
+redIADS = SkynetIADS:create('FLUG')
 
 
 ---debug settings remove from here on if you do not wan't any output on what the IADS is doing by default
@@ -10,11 +10,16 @@ iadsDebug.IADSStatus = true
 iadsDebug.radarWentDark = true
 iadsDebug.contacts = true
 iadsDebug.radarWentLive = true
-iadsDebug.noWorkingCommmandCenter = true
-iadsDebug.samNoConnection = true
+iadsDebug.noWorkingCommmandCenter = false
+iadsDebug.ewRadarNoConnection = false
+iadsDebug.samNoConnection = false
 iadsDebug.jammerProbability = true
-iadsDebug.addedEWRadar = true
+iadsDebug.addedEWRadar = false
+iadsDebug.hasNoPower = false
 iadsDebug.harmDefence = true
+iadsDebug.samSiteStatusEnvOutput = true
+iadsDebug.earlyWarningRadarStatusEnvOutput = true
+iadsDebug.commandCenterStatusEnvOutput = true
 ---end remove debug ---
 
 
