9080518b04e53e1305c67a921c718ccf1ae40c69
132nd.Neck <neck.dcs@gmail.com>
2021-11-20 22:29:38 +0100

    chg: Troubleshoot skynet

--- l10n/DEFAULT/skynet-iads-setup-FLUG.lua
+++ l10n/DEFAULT/skynet-iads-setup-FLUG.lua
@@ -3,6 +3,20 @@ do
 redIADSA = SkynetIADS:create(FLUG)
 
 
+---debug settings remove from here on if you do not wan't any output on what the IADS is doing by default
+local iadsDebug = redIADS:getDebugSettings()
+iadsDebug.IADSStatus = true
+iadsDebug.radarWentDark = true
+iadsDebug.contacts = true
+iadsDebug.radarWentLive = true
+iadsDebug.noWorkingCommmandCenter = true
+iadsDebug.samNoConnection = true
+iadsDebug.jammerProbability = true
+iadsDebug.addedEWRadar = true
+iadsDebug.harmDefence = true
+---end remove debug ---
+
+
 --add all units with unit name beginning with 'EW' to the IADS:
 redIADSA:addEarlyWarningRadarsByPrefix('EWA')
 
@@ -19,7 +33,11 @@ redIADS:getSAMSiteByGroupName('IADSA_SA2'):addPointDefence(sa15):setHARMDetectio
 local sa15 = redIADS:getSAMSiteByGroupName('IADSA_SA15_PD_2')
 redIADS:getSAMSiteByGroupName('IADSA_SA2'):addPointDefence(sa15):setHARMDetectionChance(100):setIgnoreHARMSWhilePointDefencesHaveAmmo(true)
 
--- activate the IADS -test-
+
+--Add threat site 6 (SA-11) to IADS
+redIADS:addSAMSite('Threatsite_6')
+
+-- activate the IADS 
 --redIADSA:activate()	
 redIADS:setupSAMSitesAndThenActivate()
 
