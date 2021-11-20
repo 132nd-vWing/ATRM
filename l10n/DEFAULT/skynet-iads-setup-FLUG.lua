e2dacd708373cb053d59723cca24bc4b681c0214
132nd.Neck <neck.dcs@gmail.com>
2021-11-20 23:30:39 +0100

    chg: Verify fix skynet

--- l10n/DEFAULT/skynet-iads-setup-FLUG.lua
+++ l10n/DEFAULT/skynet-iads-setup-FLUG.lua
@@ -4,25 +4,6 @@ do
 redIADS = SkynetIADS:create('FLUG')
 
 
----debug settings remove from here on if you do not wan't any output on what the IADS is doing by default
-local iadsDebug = redIADS:getDebugSettings()
-iadsDebug.IADSStatus = true
-iadsDebug.radarWentDark = true
-iadsDebug.contacts = true
-iadsDebug.radarWentLive = true
-iadsDebug.noWorkingCommmandCenter = false
-iadsDebug.ewRadarNoConnection = false
-iadsDebug.samNoConnection = false
-iadsDebug.jammerProbability = true
-iadsDebug.addedEWRadar = false
-iadsDebug.hasNoPower = false
-iadsDebug.harmDefence = true
-iadsDebug.samSiteStatusEnvOutput = true
-iadsDebug.earlyWarningRadarStatusEnvOutput = true
-iadsDebug.commandCenterStatusEnvOutput = true
----end remove debug ---
-
-
 --add all units with unit name beginning with 'EW' to the IADS:
 redIADS:addEarlyWarningRadarsByPrefix('EWA')
 
@@ -38,6 +19,10 @@ redIADS:getSAMSiteByGroupName('IADSA_SA2'):addPointDefence(sa15):setHARMDetectio
 
 
 local sa15 = redIADS:getSAMSiteByGroupName('IADSA_SA15_PD_2')
+redIADS:getSAMSiteByGroupName('IADSA_SA2'):addPointDefence(sa15):setHARMDetectionChance(100):setIgnoreHARMSWhilePointDefencesHaveAmmo(true)
+
+
+local sa15 = redIADS:getSAMSiteByGroupName('IADSA_SA15_PD_3')
 redIADS:getSAMSiteByGroupName('IADSA_SA2'):addPointDefence(sa15):setHARMDetectionChance(100):setIgnoreHARMSWhilePointDefencesHaveAmmo(true)
 
 
