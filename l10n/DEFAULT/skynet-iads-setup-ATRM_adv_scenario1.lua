d699906f295670e1035fffa9c5cca5ceeeba9f56
132nd.Neck <neck.dcs@gmail.com>
2022-05-21 16:11:41 +0200

    chg: looking into skynet

--- l10n/DEFAULT/skynet-iads-setup-ATRM_adv_scenario1.lua
+++ l10n/DEFAULT/skynet-iads-setup-ATRM_adv_scenario1.lua
@@ -3,6 +3,18 @@ do
 --create an instance of the IADS
 redIADS = SkynetIADS:create('ADV_SCENARIO_1')
 
+local iadsDebug = redIADS:getDebugSettings()
+iadsDebug.IADSStatus = true
+iadsDebug.radarWentDark = true
+iadsDebug.contacts = true
+iadsDebug.radarWentLive = true
+iadsDebug.noWorkingCommmandCenter = true
+iadsDebug.samNoConnection = true
+iadsDebug.jammerProbability = true
+iadsDebug.addedEWRadar = true
+iadsDebug.harmDefence = true
+---end remove debug ---
+
 
 --add all units with unit name beginning with 'EW' to the IADS:
 redIADS:addEarlyWarningRadarsByPrefix('ADV_SCEN_1_SBORKA')
@@ -14,7 +26,8 @@ redIADS:addSAMSitesByPrefix('ADV_SCEN_1_SAM')
 
 ewRadarOrSamSite:setAutonomousBehaviour(SkynetIADSAbstractRadarElement.AUTONOMOUS_STATE_DCS_AI)
 
-
+--activate the radio menu to toggle IADS Status output
+redIADS:addRadioMenu()
 
 
 -- activate the IADS 
