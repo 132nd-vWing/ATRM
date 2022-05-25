fca1c47ac81a953e1dedde368e6c0fc2aad7b21b
132nd.Neck <neck.dcs@gmail.com>
2022-05-25 13:26:31 +0200

    chg: Fixes to skynet advanced scenario. More lethal IADS

--- l10n/DEFAULT/skynet-iads-setup-ATRM_adv_scenario1.lua
+++ l10n/DEFAULT/skynet-iads-setup-ATRM_adv_scenario1.lua
@@ -3,17 +3,7 @@ do
 --create an instance of the IADS
 redIADS = SkynetIADS:create('ADV_SCENARIO_1')
 
-local iadsDebug = redIADS:getDebugSettings()
-iadsDebug.IADSStatus = true
-iadsDebug.radarWentDark = true
-iadsDebug.contacts = true
-iadsDebug.radarWentLive = true
-iadsDebug.noWorkingCommmandCenter = true
-iadsDebug.samNoConnection = true
-iadsDebug.jammerProbability = true
-iadsDebug.addedEWRadar = true
-iadsDebug.harmDefence = true
----end remove debug ---
+
 
 
 --add all units with unit name beginning with 'EW' to the IADS:
@@ -26,8 +16,29 @@ redIADS:addSAMSitesByPrefix('ADV_SCEN_1_SAM')
 
 redIADS:ewRadarOrSamSite:setAutonomousBehaviour(SkynetIADSAbstractRadarElement.AUTONOMOUS_STATE_DCS_AI)
 
---activate the radio menu to toggle IADS Status output
-redIADS:addRadioMenu()
+
+-- Add SA15 as point defense to SA-11
+--first get the SAM site you want to use as point defence from the IADS:
+local sa15 = redIADS:getSAMSiteByGroupName('ADV_SCEN_1_SAM_SA_15-3')
+--then add it to the SAM site it should protect:
+redIADS:getSAMSiteByGroupName('ADV_SCEN_1_SAM_SA_11_Battery'):addPointDefence(sa15)
+
+-- Add SA15 as point defense to EW Sborka
+local sa15 = redIADS:getSAMSiteByGroupName('ADV_SCEN_1_SAM_SA_15-2')
+redIADS:getEarlyWarningRadarByUnitName('ADV_SCEN_1_SBORKA'):addPointDefence(sa15)
+
+-- Add SA15 as point defense to EW Sborka
+local sa15 = redIADS:getSAMSiteByGroupName('ADV_SCEN_1_SAM_SA_15')
+redIADS:getEarlyWarningRadarByUnitName('ADV_SCEN_1_SBORKA'):addPointDefence(sa15)
+
+
+--set this SA-11 site to go live 70% of max range of its missiles (default value: 100%), its HARM detection probability is set to 50% (default value: 70%)
+redIADS:getSAMSiteByGroupName('ADV_SCEN_1_SAM_SA_11_Battery'):setGoLiveRangeInPercent(80):setHARMDetectionChance(50)
+
+redIADS:getSAMSiteByGroupName('ADV_SCEN_1_SAM_SA_8-1'):setGoLiveRangeInPercent(85):setHARMDetectionChance(90)
+redIADS:getSAMSiteByGroupName('ADV_SCEN_1_SAM_SA_8-2'):setGoLiveRangeInPercent(85):setHARMDetectionChance(90)
+redIADS:getSAMSiteByGroupName('ADV_SCEN_1_SAM_SA_8-3'):setGoLiveRangeInPercent(85):setHARMDetectionChance(90)
+redIADS:getSAMSiteByGroupName('ADV_SCEN_1_SAM_SA_8-4'):setGoLiveRangeInPercent(85):setHARMDetectionChance(90)
 
 
 -- activate the IADS 
