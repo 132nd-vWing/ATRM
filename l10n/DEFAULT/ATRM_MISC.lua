daf49199423dbee1c9991bb0d15e61e235c6d156
132nd-Entropy <entropysg@gmail.com>
2023-10-03 10:07:27 +0200

    CHG: update latest MOOSE with fixes to MessageToAll Function that broke previous scripts. CHG: remove Range14 CAS menus after a scenario has been started

--- l10n/DEFAULT/ATRM_MISC.lua
+++ l10n/DEFAULT/ATRM_MISC.lua
@@ -1,5 +1,17 @@
 _SETTINGS:SetPlayerMenuOff()
 
+---important: the MessageToAll function was removed from Moose, we add it back here, else all other scripts will break. 
+
+function MessageToAll( MsgText, MsgTime, MsgName )
+  -- trace.f()
+
+  MESSAGE:New( MsgText, MsgTime, "Message" ):ToCoalition( coalition.side.RED ):ToCoalition( coalition.side.BLUE )
+end
+
+
+
+
+
 awacs_root_menu = MENU_MISSION:New("AWACS TANKER and ELINT Control")
 range_root_menu = MENU_MISSION:New("RANGE Control")
 RANGE.MenuF10Root=MENU_MISSION:New("Basic Ranges",range_root_menu).MenuPath
